5598e767b3975b32efff67bda4ea23e6a7a4883a
PhilosHimm <161180159+PhilosHimm@users.noreply.github.com>
2024-10-07 21:54:31 -0400

    lab5

--- Lab5/lab5.js
+++ Lab5/lab5.js
@@ -5,7 +5,10 @@ console.log('Hello world!');
 window.alert('hello world!');
 */
 
-function sayHello(){
-    alert("Hello world");
+function askName(){
+    let userName = prompt("Please enter your name:");
+    
+    if (userName != null && userName.trim() !== "") 
+        document.getElementById("nameDisplay").textContent = userName;
 }
 
